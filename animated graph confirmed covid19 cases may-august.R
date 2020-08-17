2104353ff5e46681ab14498ed2c1cfd527b3f47f
juka19 <56269720+juka19@users.noreply.github.com>
2020-08-17 17:41:03 +0200

    Add files via upload

---
+++ animated graph confirmed covid19 cases may-august.R
@@ -0,0 +1,37 @@
+# Animated graph of confirmed covid19 cases in 4 European countries
+
+library(covid19.analytics)
+library(tidyverse)
+library(lubridate)
+library(ggpot2)
+library(gganimate)
+
+# dowloading updated covid19 data
+
+tsc <- covid19.data(case = "ts-confirmed")
+View(tsc)
+
+# data wrangling
+tsc <- tsc %>% 
+  filter(Country.Region %in% c("Germany", "France", 
+                               "Italy", "United Kingdom")) %>% 
+  filter(Province.State == "") %>% 
+  pivot_longer(cols = starts_with("2020"), names_to = "Date",
+               values_to = "Confirmed")
+
+tsc$Date <- as.Date(tsc$Date)
+names(tsc)[2] <- "Country"
+
+# filter for months after may and create the plot
+tsc %>% 
+  filter(Date >= "2020-05-01") %>%
+  ggplot(aes(Date, Confirmed, group = Country,
+             color = Country)) +
+  geom_line(size = 1) +
+  geom_point(size = 1) +
+  theme_bw() +
+  ggtitle("Confirmed Cases of Covid19 since May") +
+  scale_x_date(date_breaks = "months", 
+               date_labels = "%B") +
+  labs(x = "Date", y = "Confirmed Cases") +
+  transition_reveal(Date) 
